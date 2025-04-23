41471cca182246871f369cd3f342f002b61fbb97
AdnanSlef <aself.school@gmail.com>
2025-04-22 20:26:31 -0400

    integrate basic approx, import sklearn

--- qiskit_based.py
+++ qiskit_based.py
@@ -1,17 +1,21 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from functools import reduce
 import scipy
 import math
-from qiskit import QuantumCircuit
+import matplotlib.pyplot as plt
+from functools import reduce
+from collections import namedtuple
+from sklearn.neighbors import KDTree
 from qiskit.quantum_info import Operator
-from qiskit.circuit.library import UnitaryGate
 from qiskit.synthesis.discrete_basis.gate_sequence import GateSequence
-from qiskit.synthesis.discrete_basis.generate_basis_approximations import generate_basic_approximations, _1q_inverses
-from qiskit.transpiler.passes.synthesis import SolovayKitaev
+from qiskit.circuit import Gate
+import qiskit.circuit.library.standard_gates as gates
 
 from sk import distance
 
+### Use Qiskit Implementation for Reference
+from qiskit.circuit.library import UnitaryGate
+from qiskit.transpiler.passes.synthesis import SolovayKitaev
+from qiskit import QuantumCircuit
 def f(U, n):
     U_gate = UnitaryGate(U)
 
@@ -31,7 +35,6 @@ def f(U, n):
     print(mul)
     print(distance(U, mul))
     return distance(U, mul)
-
 ###
 
 def _recurse(basic_approximations, sequence: GateSequence, n: int) -> GateSequence:
@@ -107,6 +110,135 @@ def _compute_rotation_between(from_vector: np.ndarray, to_vector: np.ndarray) ->
     cross = _cross_product_matrix(np.cross(from_vector, to_vector))
     rotation_matrix = np.identity(3) + cross + np.dot(cross, cross) / (1 + dot)
     return rotation_matrix
+
+Node = namedtuple("Node", ("labels", "sequence", "children"))
+_1q_inverses = {
+    "i": "i",
+    "x": "x",
+    "y": "y",
+    "z": "z",
+    "h": "h",
+    "t": "tdg",
+    "tdg": "t",
+    "s": "sdg",
+    "sdg": "s",
+    "sx": "sxdg",
+    "sxdg": "sx",
+}
+_1q_gates = {
+    "i": gates.IGate(),
+    "x": gates.XGate(),
+    "y": gates.YGate(),
+    "z": gates.ZGate(),
+    "h": gates.HGate(),
+    "t": gates.TGate(),
+    "tdg": gates.TdgGate(),
+    "s": gates.SGate(),
+    "sdg": gates.SdgGate(),
+    "sx": gates.SXGate(),
+    "sxdg": gates.SXdgGate(),
+}
+def matrix_equal(mat1, mat2, ignore_phase=False, rtol=1e-5, atol=1e-8, props=None):
+    if not isinstance(mat1, np.ndarray):
+        mat1 = np.array(mat1)
+    if not isinstance(mat2, np.ndarray):
+        mat2 = np.array(mat2)
+
+    if mat1.shape != mat2.shape:
+        return False
+
+    if ignore_phase:
+        phase_difference = 0
+
+        # Get phase of first non-zero entry of mat1 and mat2
+        # and multiply all entries by the conjugate
+        for elt in mat1.flat:
+            if abs(elt) > atol:
+                angle = np.angle(elt)
+                phase_difference -= angle
+                mat1 = np.exp(-1j * angle) * mat1
+                break
+        for elt in mat2.flat:
+            if abs(elt) > atol:
+                angle = np.angle(elt)
+                phase_difference += angle
+                mat2 = np.exp(-1j * np.angle(elt)) * mat2
+                break
+        if props is not None:
+            props["phase_difference"] = phase_difference
+
+    return np.allclose(mat1, mat2, rtol=rtol, atol=atol)
+def _check_candidate_greedy(candidate, existing_sequences, tol=1e-10):
+    # do a quick, string-based check if the same sequence already exists
+    if any(candidate.name == existing.name for existing in existing_sequences):
+        return False
+
+    for existing in existing_sequences:
+        if matrix_equal(existing.product_su2, candidate.product_su2, ignore_phase=True, atol=tol):
+            # is the new sequence less or more efficient?
+            return len(candidate.gates) < len(existing.gates)
+    return True
+def _check_candidate_kdtree(candidate, existing_sequences, tol=1e-10):
+    # do a quick, string-based check if the same sequence already exists
+    if any(candidate.name == existing.name for existing in existing_sequences):
+        return False
+
+    points = np.array([sequence.product.flatten() for sequence in existing_sequences])
+    candidate = np.array([candidate.product.flatten()])
+
+    kdtree = KDTree(points)
+    dist, _ = kdtree.query(candidate)
+
+    return dist[0][0] > tol
+def generate_basic_approximations(basis_gates: list[str | Gate], depth: int, filename: str | None = None) -> list[GateSequence]:
+    def _process_node(node: Node, basis: list[str], sequences: list[GateSequence]):
+        inverse_last = _1q_inverses[node.labels[-1]] if node.labels else None
+
+        for label in basis:
+            if label == inverse_last:
+                continue
+
+            sequence = node.sequence.copy()
+            sequence.append(_1q_gates[label])
+
+            if _check_candidate(sequence, sequences):
+                sequences.append(sequence)
+                node.children.append(Node(node.labels + (label,), sequence, []))
+
+        return node.children
+    def _check_candidate(candidate, existing_sequences, tol=1e-10):
+        USE_KDTREE = False
+        if USE_KDTREE:
+            return _check_candidate_kdtree(candidate, existing_sequences, tol)
+        else:
+            return _check_candidate_greedy(candidate, existing_sequences, tol)
+    basis = []
+    for gate in basis_gates:
+        if isinstance(gate, str):
+            if gate not in _1q_gates:
+                raise ValueError(f"Invalid gate identifier: {gate}")
+            basis.append(gate)
+        else:  # gate is a qiskit.circuit.Gate
+            basis.append(gate.name)
+
+    tree = Node((), GateSequence(), [])
+    cur_level = [tree]
+    sequences = [tree.sequence]
+    for _ in [None] * depth:
+        next_level = []
+        for node in cur_level:
+            next_level.extend(_process_node(node, basis, sequences))
+        cur_level = next_level
+
+    if filename is not None:
+        data = {}
+        for sequence in sequences:
+            gatestring = sequence.name
+            data[gatestring] = (sequence.product, sequence.global_phase)
+
+        np.save(filename, data)
+
+    return sequences
 
 def commutator_decompose(u_so3: np.ndarray) -> tuple[GateSequence, GateSequence]:
     angle = _solve_decomposition_angle(u_so3)
